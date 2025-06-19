73bc9956a88b48d3e89b06afd597d0dc4e4cebdd
Aditya Chavan <chavanaditya9325@gmail.com>
2025-06-19 21:23:06 +0530

    Enhance playlist creation and retrieval with validation and error handling

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -6,14 +6,55 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const createPlaylist = asyncHandler(async (req, res) => {
-    const {name, description} = req.body
-
+    const {name, description, videoId} = req.body
     //TODO: create playlist
+
+    if(!name || !description){
+        throw new ApiError(400, "Name and description are must required")
+    }
+
+    const playlist = await Playlist.create({
+        name,
+        description, 
+        videos: videoId ? [videoId] : [], 
+        owner: req.user?._id
+    }) 
+
+    if(!playlist){
+        throw new ApiError(500, "failed to create plalist")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(200, playlist, "playlist created successfull")
+    )
+
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+
+    if(!userId){
+        throw new ApiError(400, "User ID is required")
+    }
+    
+    const playlists  = await Playlist.find({owner: userId})
+    .populate("owner", "username avatar")
+    .populate("videos", "title thumbnail")
+    .sort({createdAt: -1})
+
+    if(!playlists || playlists.length === 0){
+        throw new ApiError(404, "No Playlists found for this user")
+    }
+
+    return res 
+    .status(200)
+    .json(
+        new ApiResponse(200, playlists, "Playlist fetched successfully")
+    )
+
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
